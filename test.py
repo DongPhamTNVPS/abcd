75fdaa7fb243635b1009a58c88be7860e28281fb
LeVanTrung <82043379+letrung8821@users.noreply.github.com>
2022-05-16 22:17:46 +0700

    trungle

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ print("hello world")
 
 print("tui push len ok, pull error")
 print ("hello")
+print("B1: git add test.py")
+print("B2: git branch -M trungle")
+print("B3: git commit -m trunglecommit")
+print("B4: git remote add test.py  https://github.com/DongPhamTNVPS/abcd.git")
+print("B5: git push test.py")
